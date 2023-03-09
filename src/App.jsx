a3506c86565c8104e16675f052b4b62d8a8bfe22
Manuel <benitessolismanuel@gmail.com>
2023-03-09 18:40:09 -0500

    gh-pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import Header from "./components/Header.jsx"
-import SearchUser from "./components/SearchUser.jsx"
-import { ThemeContextProvider } from "./context/ThemeContext.jsx"
+import Search from "./components/Search.jsx"
 import { useThemes } from "./context/ThemeContext.jsx"
 
 function App() {
 	const { theme } = useThemes()
 	return (
-		<div className={"App" + " " + "bg-" + theme + "-mode"}>
+		<div className={"App" + " " + "bg-" + theme}>
 			<div className="app__container">
 				<Header />
-				<SearchUser />
+				<Search />
 			</div>
 		</div>
 	)
